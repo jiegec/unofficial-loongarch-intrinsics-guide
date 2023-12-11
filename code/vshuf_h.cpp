742a795eda14313aba7f97a036bdc45a337be3a7
Jiajie Chen <c@jia.je>
2023-12-12 01:02:51 +0800

    Add more testing coe

---
+++ code/vshuf_h.cpp
@@ -0,0 +1,31 @@
+#include "common.h"
+
+void test() {
+  __m128i b = {0x123456789ABCDEF0, 0x0FEDCBA987654321};
+  __m128i c = {0x1122334455667788, 0x1212343456567878};
+  __m128i a = {0x0001020304050607, 0x0001020304050607};
+  PRINT(a);
+  PRINT(b);
+  PRINT(c);
+  PRINT(__lsx_vshuf_h(a, b, c));
+
+  a = __m128i {0x08090A0B0C0D0E0F, 0x08090A0B0C0D0E0F};
+  PRINT(a);
+  PRINT(__lsx_vshuf_h(a, b, c));
+
+  a = __m128i {0x1011121314151617, 0x1011121314151617};
+  PRINT(a);
+  PRINT(__lsx_vshuf_h(a, b, c));
+
+  a = __m128i {0x18191A1B1C1D1E1F, 0x18191A1B1C1D1E1F};
+  PRINT(a);
+  PRINT(__lsx_vshuf_h(a, b, c));
+
+  a = __m128i {0x2021FFFFFFFFFFFF, 0x2223FFFFFFFFFFFF};
+  PRINT(a);
+  PRINT(__lsx_vshuf_h(a, b, c));
+
+  a = __m128i {0x0706050403020100, 0x1F1E1D1C1B1A1918};
+  PRINT(a);
+  PRINT(__lsx_vshuf_h(a, b, c));
+}
