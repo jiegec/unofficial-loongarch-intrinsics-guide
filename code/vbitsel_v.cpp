eadb5bd78d6325a598fe8c1eb9b51104a6e08d18
Jiajie Chen <c@jia.je>
2023-12-12 10:17:26 +0800

    Implement vbitsel_v

--- code/vbitsel_v.cpp
+++ code/vbitsel_v.cpp
@@ -1,5 +1,13 @@
 #include "common.h"
 
+v128 vbitsel_v(v128 a, v128 b, v128 c) {
+  v128 dst;
+  for (int i = 0; i < 2; i++) {
+    dst.dword[i] = (c.dword[i] & b.dword[i]) | (~c.dword[i] & a.dword[i]);
+  }
+  return dst;
+}
+
 void test() {
   __m128i a = {0x123456789ABCDEF0, 0x0FEDCBA987654321};
   __m128i b = {0x1122334455667788, 0x1212343456567878};
@@ -8,4 +16,14 @@ void test() {
   PRINT(b);
   PRINT(c);
   PRINT(__lsx_vbitsel_v(a, b, c));
+
+  for (int i = 0; i < 64; i++) {
+    v128 a, b, c;
+    PRINT(a);
+    PRINT(b);
+    PRINT(c);
+    PRINT(__lsx_vbitsel_v(a, b, c));
+    PRINT(vbitsel_v(a, b, c));
+    assert(vbitsel_v(a, b, c) == __lsx_vbitsel_v(a, b, c));
+  }
 }
