257039ca888192e2583440fc0fae0f35ffbdd2d4
Jiajie Chen <c@jia.je>
2023-12-12 11:47:20 +0800

    Reduce duplication by using macros

---
+++ main.py
@@ -0,0 +1,71 @@
+def define_env(env):
+    widths = {
+        "b": 8,
+        "bu": 8,
+        "h": 16,
+        "hu": 16,
+        "w": 32,
+        "wu": 32,
+        "d": 64,
+        "du": 64,
+        "q": 128,
+    }
+
+    signednesses = {
+        "b": "signed",
+        "bu": "unsigned",
+        "h": "signed",
+        "hu": "unsigned",
+        "w": "signed",
+        "wu": "unsigned",
+        "d": "signed",
+        "du": "unsigned",
+        "q": "signed",
+    }
+
+    def include(file):
+        return open(f"code/{file}").read().strip()
+
+    def instruction(intrinsic, instr, desc):
+        file_name = instr.split(" ")[0].replace(".", "_")
+        return f"""
+## {intrinsic}
+
+### Synopsis
+
+```c++
+{intrinsic}
+#include <lsxintrin.h>
+Instruction: {instr}
+CPU Flags: LSX
+```
+
+### Description
+
+{desc}
+
+### Operation
+
+```c++
+{include(f'{file_name}.h')}
+```
+"""
+
+    @env.macro
+    def vadd(name):
+        width = widths[name]
+        return instruction(
+            intrinsic=f"__m128i __lsx_vadd_{name} (__m128i a, __m128i b)",
+            instr=f"vadd.{name} vr, vr, vr",
+            desc=f"Add {width}-bit elements in `a` and `b`, save the result in `dst`.",
+        )
+
+    @env.macro
+    def vabsd(name):
+        width = widths[name]
+        signedness = signednesses[name]
+        return instruction(
+            intrinsic=f"__m128i __lsx_vabsd_{name} (__m128i a, __m128i b)",
+            instr=f"vabsd.{name} vr, vr, vr",
+            desc=f"Compute absolute difference of {signedness} {width}-bit elements in `a` and `b`, save the result in `dst`.",
+        )
