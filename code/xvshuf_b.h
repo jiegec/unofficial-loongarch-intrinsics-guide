18f664a3423432f2fe8bc5ffe5cc6fdc357558b3
Jiajie Chen <c@jia.je>
2023-12-14 14:08:07 +0800

    Add 3C5000 caveat

--- code/xvshuf_b.h
+++ code/xvshuf_b.h
@@ -1,5 +1,6 @@
 for (int i = 0; i < 32; i++) {
   if ((c.byte[i] % 256) >= 64 && MACHINE_3C5000) {
+    // Caveat: observed in 3C5000
     dst.byte[i] = 0;
   } else if ((c.byte[i] % 32) < 16) {
     dst.byte[i] = b.byte[(c.byte[i] % 32) + ((i >= 16) ? 16 : 0)];
--- code/xvshuf_d.h
+++ code/xvshuf_d.h
@@ -1,5 +1,6 @@
 for (int i = 0; i < 4; i++) {
   if ((a.dword[i] % 256) >= 64 && MACHINE_3C5000) {
+    // Caveat: observed in 3C5000
     dst.dword[i] = 0;
   } else if ((a.dword[i] % 4) < 2) {
     dst.dword[i] = c.dword[(a.dword[i] % 4) + ((i >= 2) ? 2 : 0)];
