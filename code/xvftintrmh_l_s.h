398bd1d0e8069ea51964c6227144b724fc443394
Jiajie Chen <c@jia.je>
2025-07-05 23:02:39 +0800

    Fix xvftinth/xvftintl variants impl

--- code/xvftintrmh_l_s.h
+++ code/xvftintrmh_l_s.h
@@ -1,3 +1,6 @@
-for (int i = 0; i < 4; i++) {
-  dst.dword[i] = (s32)a.fp32[i + 4]; // rounding mode is not expressed in C
+for (int i = 0; i < 2; i++) {
+  dst.dword[i] = (s64)a.fp32[i + 2]; // rounding mode is not expressed in C
+}
+for (int i = 2; i < 4; i++) {
+  dst.dword[i] = (s64)a.fp32[i + 4]; // rounding mode is not expressed in C
 }
