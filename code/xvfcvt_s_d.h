ec93a50e69f408f783f5eceb6b20aabe11d56e0f
Jiajie Chen <c@jia.je>
2025-07-06 18:37:38 +0800

    Format code

--- code/xvfcvt_s_d.h
+++ code/xvfcvt_s_d.h
@@ -1,9 +1,9 @@
 for (int i = 0; i < 8; i++) {
   if (i < 2) {
     dst.fp32[i] = b.fp64[i];
-  } else if(i < 4){
+  } else if (i < 4) {
     dst.fp32[i] = a.fp64[i - 2];
-  } else if(i < 6){
+  } else if (i < 6) {
     dst.fp32[i] = b.fp64[i - 2];
   } else {
     dst.fp32[i] = a.fp64[i - 4];
