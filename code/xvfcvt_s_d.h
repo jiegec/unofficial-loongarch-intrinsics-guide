a57f219895912d3651ff3ba1575da2ce912907e5
Jiajie Chen <c@jia.je>
2025-07-06 18:35:49 +0800

    Fix xvfcvt_s_d impl for issue #23

--- code/xvfcvt_s_d.h
+++ code/xvfcvt_s_d.h
@@ -1,6 +1,10 @@
 for (int i = 0; i < 8; i++) {
-  if (i < 4) {
+  if (i < 2) {
     dst.fp32[i] = b.fp64[i];
+  } else if(i < 4){
+    dst.fp32[i] = a.fp64[i - 2];
+  } else if(i < 6){
+    dst.fp32[i] = b.fp64[i - 2];
   } else {
     dst.fp32[i] = a.fp64[i - 4];
   }
