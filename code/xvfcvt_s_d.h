2c1038e2eed32ee4e6618052776e7925aa31b547
Jiajie Chen <c@jia.je>
2025-07-06 18:41:39 +0800

    Add comments to clarity

--- code/xvfcvt_s_d.h
+++ code/xvfcvt_s_d.h
@@ -1,11 +1,16 @@
-for (int i = 0; i < 8; i++) {
-  if (i < 2) {
-    dst.fp32[i] = b.fp64[i];
-  } else if (i < 4) {
-    dst.fp32[i] = a.fp64[i - 2];
-  } else if (i < 6) {
-    dst.fp32[i] = b.fp64[i - 2];
-  } else {
-    dst.fp32[i] = a.fp64[i - 4];
-  }
+for (int i = 0; i < 2; i++) {
+  // dst[0,1] <- b[0,1]
+  dst.fp32[i] = b.fp64[i];
+}
+for (int i = 2; i < 4; i++) {
+  // dst[2,3] <- a[0,1]
+  dst.fp32[i] = a.fp64[i - 2];
+}
+for (int i = 4; i < 6; i++) {
+  // dst[4,5] <- b[2,3]
+  dst.fp32[i] = b.fp64[i - 2];
+}
+for (int i = 6; i < 8; i++) {
+  // dst[6,7] <- a[2,3]
+  dst.fp32[i] = a.fp64[i - 4];
 }
