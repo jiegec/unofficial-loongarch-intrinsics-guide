4a802dd5f5f364ce3a31337381b3491823a91782
Jiajie Chen <c@jia.je>
2025-07-07 08:37:55 +0800

    Fix xvfcvt_h_s impl for issue #23

--- code/xvfcvt_h_s.h
+++ code/xvfcvt_h_s.h
@@ -1,7 +1,16 @@
-for (int i = 0; i < 16; i++) {
-  if (i < 8) {
-    dst.fp16[i] = b.fp32[i];
-  } else {
-    dst.fp16[i] = a.fp32[i - 8];
-  }
+for (int i = 0; i < 4; i++) {
+  // dst[0-3] <- b[0-3]
+  dst.fp16[i] = b.fp32[i];
+}
+for (int i = 4; i < 8; i++) {
+  // dst[4-7] <- a[0-3]
+  dst.fp16[i] = a.fp32[i - 4];
+}
+for (int i = 8; i < 12; i++) {
+  // dst[8-11] <- b[4-7]
+  dst.fp16[i] = b.fp32[i - 4];
+}
+for (int i = 12; i < 16; i++) {
+  // dst[12-15] <- a[4-7]
+  dst.fp16[i] = a.fp32[i - 8];
 }
