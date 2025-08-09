baeef07b407df07cb4220339beac22484eb2939f
Jiajie Chen <c@jia.je>
2025-08-09 22:23:45 +0800

    Simplify code

--- code/xvhseli_d.h
+++ code/xvhseli_d.h
@@ -1,4 +1,4 @@
-dst.dword[0] = a.dword[(imm & 1) >> 0];
-dst.dword[1] = a.dword[(imm & 2) >> 1];
-dst.dword[2] = a.dword[2 + ((imm & 4) >> 2)];
-dst.dword[3] = a.dword[2 + ((imm & 8) >> 3)];
+dst.dword[0] = (imm & 1) ? a.dword[1] : a.dword[0];
+dst.dword[1] = (imm & 2) ? a.dword[1] : a.dword[0];
+dst.dword[2] = (imm & 4) ? a.dword[3] : a.dword[2];
+dst.dword[3] = (imm & 8) ? a.dword[3] : a.dword[2];
