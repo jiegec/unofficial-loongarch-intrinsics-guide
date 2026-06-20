b5a52a4ecddc8d63fb2acabf8b28c5686ce1a8bd
phorcys <phorcys@126.com>
2026-06-21 01:06:12 +0800

    Add armsrai_w impl

---
+++ code/armsrai_w.cpp
@@ -0,0 +1,28 @@
+#include "common.h"
+
+uint64_t armsrai_w(eflags &ARMFLAGS, uint64_t a, int imm, int cond) {
+  uint64_t dst = 0;
+#include "armsrai_w.h"
+  return dst;
+}
+
+#define ref_armsrai_w(eflags, a, imm, cond)                                   \
+  ({                                                                           \
+    uint16_t flags = eflags.raw;                                               \
+    asm volatile(                                                              \
+        "x86mtflag %0, 0x3f\narmsrai.w %1, %2, %3\nx86mfflag %0, 0x3f"        \
+        : "+r"(flags)                                                          \
+        : "r"(a), "n"(imm), "n"(cond)                                          \
+        : "memory");                                                           \
+    eflags.raw = flags;                                                        \
+    0;                                                                         \
+  })
+
+void test() {
+  IFUZZ1(armsrai_w, 0, 7);
+  IFUZZ1(armsrai_w, 0, 7);
+  IFUZZ1(armsrai_w, 15, 14);
+  IFUZZ1(armsrai_w, 15, 15);
+  IFUZZ1(armsrai_w, 31, 15);
+  IFUZZ1(armsrai_w, 31, 15);
+}
