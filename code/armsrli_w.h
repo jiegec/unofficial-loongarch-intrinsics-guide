dd67776c2e1bbe49f7bacfba97552bd8cc175391
phorcys <phorcys@126.com>
2026-06-21 01:07:41 +0800

    Add armsrli_w impl

---
+++ code/armsrli_w.h
@@ -0,0 +1,7 @@
+if (arm_cond_holds(ARMFLAGS, cond)) {
+  uint32_t v = (uint32_t)a;
+  uint32_t r = v >> imm;
+  ARMFLAGS.C = imm == 0 ? ARMFLAGS.C : (imm > 32 ? 0 : ((v >> (imm - 1)) & 1));
+  ARMFLAGS.N = (r >> 31) & 1;
+  ARMFLAGS.Z = r == 0;
+}
