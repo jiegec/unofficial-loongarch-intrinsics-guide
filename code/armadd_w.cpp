b7168a6f3ee3aeb6123f8123a7295002053be510
phorcys <phorcys@126.com>
2026-06-20 23:53:25 +0800

    Add armadd_w impl

--- code/armadd_w.cpp
+++ code/armadd_w.cpp
@@ -1,15 +1,15 @@
 #include "common.h"
 
-uint64_t armadc_w(eflags &ARMFLAGS, uint64_t a, uint64_t b, int cond) {
+uint64_t armadd_w(eflags &ARMFLAGS, uint64_t a, uint64_t b, int cond) {
   uint64_t dst = 0;
-#include "armadc_w.h"
+#include "armadd_w.h"
   return dst;
 }
 
-#define ref_armadc_w(eflags, a, b, cond)                                       \
+#define ref_armadd_w(eflags, a, b, cond)                                       \
   ({                                                                           \
     uint16_t flags = eflags.raw;                                               \
-    asm volatile("x86mtflag %0, 0x3f\narmadc.w %1, %2, %3\nx86mfflag %0, 0x3f" \
+    asm volatile("x86mtflag %0, 0x3f\narmadd.w %1, %2, %3\nx86mfflag %0, 0x3f" \
                  : "+r"(flags)                                                 \
                  : "r"(a), "r"(b), "n"(cond)                                   \
                  : "memory");                                                  \
@@ -18,8 +18,8 @@ uint64_t armadc_w(eflags &ARMFLAGS, uint64_t a, uint64_t b, int cond) {
   })
 
 void test() {
-  IFUZZ2(armadc_w, 0);
-  IFUZZ2(armadc_w, 7);
-  IFUZZ2(armadc_w, 14);
-  IFUZZ2(armadc_w, 15);
+  IFUZZ2(armadd_w, 0);
+  IFUZZ2(armadd_w, 7);
+  IFUZZ2(armadd_w, 14);
+  IFUZZ2(armadd_w, 15);
 }
