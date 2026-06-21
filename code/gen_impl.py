b0a9e1732a9e57afbb26b0e66e660ed7dcc0c9bc
phorcys <phorcys@126.com>
2026-06-21 23:02:55 +0800

    Add x86rcri impl

--- code/gen_impl.py
+++ code/gen_impl.py
@@ -2238,6 +2238,22 @@ for width, (bits, utype, stype, umax, smin, smax, msb) in x86_w.items():
         print(f"    }}", file=f)
         print(f"}}", file=f)
 
+    # x86rcri (same as rcr but with immediate)
+    with open(f"x86rcri_{width}.h", "w") as f:
+        print(f"{utype} v = ({utype})a;", file=f)
+        print(f"unsigned c = (unsigned)(imm & {rcl_mask});", file=f)
+        print(f"unsigned n = c{rcl_mod};", file=f)
+        print(f"if (n != 0) {{", file=f)
+        print(f"    unsigned carry_in = EFLAGS.CF;", file=f)
+        print(f"    unsigned carry_out = ((v >> ((n - 1) % ({bits} + 1))) & 1);", file=f)
+        print(f"    EFLAGS.CF = carry_out;", file=f)
+        print(f"    if (c == 1) {{", file=f)
+        carry_shift = f"((uint{bits}_t)carry_in << {bits - 1})" if bits == 64 else f"(carry_in << {bits - 1})"
+        print(f"        {utype} r = ({utype})((v >> 1) | {carry_shift});", file=f)
+        print(f"        EFLAGS.OF = (((r >> {bits - 1}) ^ (r >> {bits - 2})) & 1) != 0;", file=f)
+        print(f"    }}", file=f)
+        print(f"}}", file=f)
+
     # x86mul (signed)
     with open(f"x86mul_{width}.h", "w") as f:
         print(f"{utype} lhs = ({utype})a;", file=f)
---
+++ code/x86rcri_d.h
@@ -0,0 +1,12 @@
+uint64_t v = (uint64_t)a;
+unsigned c = (unsigned)(imm & 0x3f);
+unsigned n = c;
+if (n != 0) {
+  unsigned carry_in = EFLAGS.CF;
+  unsigned carry_out = ((v >> ((n - 1) % (64 + 1))) & 1);
+  EFLAGS.CF = carry_out;
+  if (c == 1) {
+    uint64_t r = (uint64_t)((v >> 1) | ((uint64_t)carry_in << 63));
+    EFLAGS.OF = (((r >> 63) ^ (r >> 62)) & 1) != 0;
+  }
+}
