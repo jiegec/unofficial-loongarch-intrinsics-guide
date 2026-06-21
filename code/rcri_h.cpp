48a53e4966c29cc4902f63ef0a69ca05fa596749
phorcys <phorcys@126.com>
2026-06-21 11:39:50 +0800

    Add rcri_h impl

---
+++ code/rcri_h.cpp
@@ -0,0 +1,26 @@
+#include "common.h"
+
+uint64_t rcri_h(eflags &EFLAGS, uint64_t a, int imm) {
+  uint64_t dst;
+#include "rcri_h.h"
+  return dst;
+}
+
+#define ref_rcri_h(eflags, a, imm)                                             \
+  ({                                                                           \
+    uint16_t _efl = (eflags).raw;                                              \
+    uint64_t _result;                                                          \
+    asm volatile("x86mtflag %1, 0x3f\nrcri.h %0, %2, %3\nx86mfflag %1, 0x3f"   \
+                 : "=r"(_result), "+r"(_efl)                                   \
+                 : "r"(a), "n"(imm)                                            \
+                 : "memory");                                                  \
+    (eflags).raw = _efl;                                                       \
+    _result;                                                                   \
+  })
+
+void test() {
+  IFUZZ1(rcri_h, 0);
+  IFUZZ1(rcri_h, 1);
+  IFUZZ1(rcri_h, 7);
+  IFUZZ1(rcri_h, 15);
+}
