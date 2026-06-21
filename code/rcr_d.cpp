41ab9e68aa405975230500e7428de3b7867ceafd
phorcys <phorcys@126.com>
2026-06-21 11:28:23 +0800

    Add rcr_d impl

---
+++ code/rcr_d.cpp
@@ -0,0 +1,20 @@
+#include "common.h"
+
+uint64_t rcr_d(eflags &EFLAGS, uint64_t a, uint64_t b) {
+  uint64_t dst;
+#include "rcr_d.h"
+  return dst;
+}
+
+uint64_t ref_rcr_d(eflags &EFLAGS, uint64_t a, uint64_t b) {
+  uint16_t eflags = EFLAGS.raw;
+  uint64_t dst;
+  asm volatile("x86mtflag %1, 0x3f\nrcr.d %0, %2, %3\nx86mfflag %1, 0x3f"
+               : "=r"(dst), "+r"(eflags)
+               : "r"(a), "r"(b)
+               : "memory");
+  EFLAGS.raw = eflags;
+  return dst;
+}
+
+void test() { IFUZZ2(rcr_d); }
